0f9419c9ed2b2e2f5ae11760d20f7813d93d8099
Valentina <valend321@gmail.com>
2023-08-22 17:31:42 -0300

    rmds

--- src/exp_1.R
+++ src/exp_1.R
@@ -8,7 +8,7 @@ library(dplyr)    # For data manipulation
 PARALLELIZE <- FALSE # Set the option for parallelization of computations
 N_THREADS <- 30     # Define the number of threads for parallel processing
 N_BINS <- 10        # Define the number of bins for discretization
-RERUN_EXP <- TRUE   # Set the option to rerun the experiment
+RERUN_EXP <- FALSE   # Set the option to rerun the experiment
 
 # Load provided functions
 source("./provided_functions.R")
